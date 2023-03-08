0461c910f750eb6d714badcb074b4ed41f3f5c65
Hygor Martins <hygor.martins74@gmail.com>
2023-03-08 00:48:28 -0300

    feat: finishing component comments

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -5,6 +5,45 @@ import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/hygorm10.png",
+      name: "Hygor Martins",
+      role: "Developer Specialist @ GB Tech",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀{" "}',
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-03-05 17:37:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/vanessasilva17.png",
+      name: "Vanessa Silva",
+      role: "Product Manager @ Banco BV",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀{" "}',
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-03-03 20:00:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -13,14 +52,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Hygor Martins"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat dolorem porro neque facilis quod in distinctio, soluta omnis maiores facere odit quia perferendis mollitia tempora sapiente ipsa animi magnam obcaecati."
-          />
-          <Post
-            author="Vanessa Silva"
-            content="Uma nova autora muito linda!!"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
